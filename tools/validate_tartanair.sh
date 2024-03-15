37793a964f4ebd227794989b84b631bb57ccf7d4
Continy <satanama.ring@gmail.com>
2024-03-15 20:50:08 +0800

    tartan1.0 test

--- tools/validate_tartanair.sh
+++ tools/validate_tartanair.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-TARTANAIR_PATH=datasets/TartanAir
+TARTANAIR_PATH=/data2/datasets/wenshanw/tartan_data
 
 python evaluation_scripts/validate_tartanair.py --datapath=$TARTANAIR_PATH --weights=droid.pth --disable_vis $@
 
