965a4c836843867b0571ef68bc75c97ff22e41a7
Continy <satanama.ring@gmail.com>
2024-05-09 13:19:54 +0800

    fix data

--- tools/validate_tartanair_v2_test.sh
+++ tools/validate_tartanair_v2_test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 
-TARTANAIR_PATH=/datasets
+TARTANAIR_PATH=/zihao/datasets
 
 python evaluation_scripts/validate_tartanair_v2_test.py --datapath=$TARTANAIR_PATH --weights=droid.pth --disable_vis $@
 
